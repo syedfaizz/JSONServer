47b7e9e124445be75ebbb2f1d3df40933cd2757d
syedfaizz <syedfaizz45@gmail.com>
2021-09-11 23:52:51 +0530

    [faisal] Add: welcome

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jasonserver_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
